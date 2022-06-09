8f304cb28a3766d58e142de7bd9a69e7dce61c71
Yan <yanzz@proton.me>
2022-06-09 14:26:22 -0400

    modify Window

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -1,13 +1,22 @@
+import { useState, useEffect } from 'react';
 import { Hello } from './apps/Hello';
 import { Counter } from './apps/Counter';
 import { Notes } from './apps/Notes';
 
 export const Window = ({ component }) => {
-  return (
-    <div className="window">
-      {component === 'hello' && <Hello />}
-      {component === 'counter' && <Counter />}
-      {component === 'notes' && <Notes />}
-    </div>
-  );
+  const [window, setWindow] = useState(<Hello />);
+
+  useEffect(() => {
+    {
+      component === 'hello' && setWindow(<Hello />);
+    }
+    {
+      component === 'counter' && setWindow(<Counter />);
+    }
+    {
+      component === 'notes' && setWindow(<Notes />);
+    }
+  }, [component]);
+
+  return <div className="window">{window}</div>;
 };
