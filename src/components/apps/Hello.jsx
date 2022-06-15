9e1400c122ec835f3d875a8baa88c059a3efe89e
Yan <yanzz@proton.me>
2022-06-15 08:49:10 -0400

    add date, time to Hello

--- src/components/apps/Hello.jsx
+++ src/components/apps/Hello.jsx
@@ -1,7 +1,16 @@
+import { useState, useEffect } from 'react';
+
 export const Hello = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    setTimeout(setDate(new Date()), 1000);
+  }, [date]);
+
   return (
     <div className="hello">
-      <h2>Hello Nurse!</h2>
+      <h1>{date.toLocaleTimeString()}</h1>
+      <h3>{date.toDateString()}</h3>
     </div>
   );
 };
