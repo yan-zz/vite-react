efd08efe6982e55f7eba77e963d32d521a08f826
Yan <yanzz@proton.me>
2022-06-16 16:59:43 -0400

    modified Notes

--- src/components/apps/Notes.jsx
+++ src/components/apps/Notes.jsx
@@ -2,14 +2,37 @@ import { useState, useEffect } from 'react';
 
 export const Notes = () => {
   const [notes, setNotes] = useState(['first']);
+  const [inputValue, setInputValue] = useState('');
 
   const handleClick = () => {
     event.preventDefault();
-    setNotes([...notes, document.getElementById('input').value]);
+    setNotes([...notes, inputValue]);
   };
 
-  const list = notes.map((note) => {
-    return <li key={note}>{note}</li>;
+  const handleChange = (event) => {
+    const inputEvent = event.target.value;
+    setInputValue(inputEvent);
+  };
+
+  const remove = (event) => {
+    event.preventDefault();
+    const toRemove = parseInt(event.target.name, 10);
+    const copyNotes = notes;
+    copyNotes.splice(toRemove, 1);
+    setNotes(copyNotes);
+  };
+
+  const list = notes.map((note, i) => {
+    return (
+      <li key={i}>
+        <button
+          name={i}
+          onClick={(event) => remove(event)}
+          children={'x'}
+        />
+        {note}
+      </li>
+    );
   });
 
   useEffect(() => {
@@ -19,7 +42,7 @@ export const Notes = () => {
   return (
     <div className="notes">
       <form>
-        <input type="text" id="input" />
+        <input type="text" onChange={handleChange} />
         <button onClick={() => handleClick()} children="add" />
       </form>
       <ul>{list}</ul>
