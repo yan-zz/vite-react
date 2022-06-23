a8ef738b558b752a28c9da6353c1f70dcbc79aec
Yan <yanzz@proton.me>
2022-06-23 11:50:57 -0400

    add Calculator

--- src/components/apps/Calculator.jsx
+++ src/components/apps/Calculator.jsx
@@ -1,3 +1,55 @@
+import { useState } from 'react';
+
 export const Calculator = () => {
-  return <h2>1+1=11</h2>;
+  const [output, setOutput] = useState('0');
+  const numbers = [
+    'AC',
+    '<',
+    '/',
+    '9',
+    '8',
+    '7',
+    '*',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '.',
+    '0',
+    '=',
+  ];
+
+  const handleClick = (value) => {
+    if (value === 'AC') setOutput('0');
+    else if (value === '<') {
+      output.slice(0, -1) === ''
+        ? setOutput('0')
+        : setOutput(output.slice(0, -1));
+    } else if (value === '=') setOutput(eval(output));
+    else
+      output === '0'
+        ? setOutput('' + value)
+        : setOutput(output + value);
+  };
+
+  return (
+    <div className="calculator">
+      <div className="output">{output}</div>
+      <div className="numbers">
+        {numbers.map((number) => (
+          <button
+            key={number + 'w'}
+            value={number}
+            onClick={(e) => handleClick(e.target.value)}
+          >
+            {number}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
